b65e7b57c05cc7c36956b2595e3d62a9014f9b95
Kamila Carvalho <kamilaingridc@gmail.com>
2025-04-24 16:32:38 -0300

    :pencil: Exercises

---
+++ src/main/java/br/com/alura/screenmatch/exercises/LessonFour.java
@@ -0,0 +1,35 @@
+package br.com.alura.screenmatch.exercises;
+
+public class LessonFour {
+//    1 - Retorne todos os produtos com o nome exato fornecido.
+//
+//2 - Retorne todos os produtos associados a uma categoria específica.
+//
+//3 - Retorne produtos com preço maior que o valor fornecido.
+//
+//4 - Retorne produtos com preço menor que o valor fornecido.
+//
+//5 - Retorne produtos cujo nome contenha o termo especificado.
+//
+//            6 - Retorne pedidos que ainda não possuem uma data de entrega.
+//
+//            7 - Retorne pedidos com data de entrega preenchida.
+//
+//8 - Retorne produtos de uma categoria ordenados pelo preço de forma crescente.
+//
+//9 - Retorne produtos de uma categoria ordenados pelo preço de forma decrescente.
+//
+//10 - Retorne a contagem de produtos em uma categoria específica.
+//
+//11 - Retorne a contagem de produtos cujo preço seja maior que o valor fornecido.
+//
+//12 - Retorne produtos com preço menor que o valor fornecido ou cujo nome contenha o termo especificado.
+//
+//            13 - Retorne pedidos feitos após uma data específica.
+//
+//14 - Retorne pedidos feitos antes de uma data específica. , 15 - Retorne pedidos feitos em um intervalo de datas.
+//
+//            16 - Retorne os três produtos mais caros.
+//
+//            17 - Retorne os cinco produtos mais baratos de uma categoria.
+}
